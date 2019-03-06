98f797c5e196e53c313daf2d6ed21cb8226ed728
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2019-03-07 06:37:16 +0900

    파일올림

--- 2-6 Nextion/1-20_nextion01/1-20_nextion01.ino
+++ 2-6 Nextion/1-20_nextion01/1-20_nextion01.ino
@@ -1,24 +1,38 @@
+#include "Nextion.h"
+
+NexText t0 = NexText(0, 1, "t0");
+NexText b0 = NexText(0, 2, "b0");
 
 int v=0;
+char msg[100]={"abcd"};
+
+NexTouch *nex_listen_list[] =
+{
+  &b0,
+  NULL
+};
+
+void b0PushCallback(void *ptr) {
+  t0.setText(msg);
+}
   
 void setup() {
   // put your setup code here, to run once:
+  nexInit();
   Serial.begin(9600);
+  Serial.println(msg);
+  b0.attachPush(b0PushCallback);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println();
-  Serial.print("t0.val="); //처음 실행문은 전송되지 않음 Error
-  Serial.print(v);  
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.print("t0.val="); 
-  Serial.print(v);  
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.write(0xff);
+  /*
+  sprintf (msg, "p0%d.bco=%d%c%c%c",i,color,0xff,0xff,0xff);
+  Serial1.print(msg);
+  Serial1.print(msg);
+  */
+  delay(30);
+  nexLoop(nex_listen_list);
   v=v+20;
-  delay(2000);
 }
