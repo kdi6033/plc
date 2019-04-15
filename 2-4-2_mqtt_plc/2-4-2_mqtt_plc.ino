49b6c2797006c6dd5d4e53b0e2ff7186e093d3b3
kdi6033 <37902752+kdi6033@users.noreply.github.com>
2019-04-15 18:08:02 +0900

    소스프로그램 수정

--- 2-4-2_mqtt_plc/2-4-2_mqtt_plc.ino
+++ 2-4-2_mqtt_plc/2-4-2_mqtt_plc.ino
@@ -57,6 +57,13 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
   // mqtt로 들어온 값을 RS232 통신 TX로 보냄
   digitalWrite(outPin, HIGH);
   delay(50);
