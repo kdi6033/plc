204d4d662c1450edf7e0525fec9647b7335223a0
kdi6033 <kdi6033@gmail.com>
2021-05-31 18:54:27 +0900

    plc

---
+++ 2-25-3 LS PLC XEC-DR14E XG5000 modbus continuous bit read&write with Arduino Esp8266 HTML - Oled - 01/plc_server_2_25_3/handleHttp.ino
@@ -0,0 +1,133 @@
+// html->string 변환사이트 다음 유튜브 참조하세요 https://youtu.be/eY4K7Wu7fgc
+// 변환 사이트
+// http://davidjwatts.com/youtube/esp8266/esp-convertHTM.html#
+// https://tomeko.net/online_tools/cpp_text_escape.php?lang=en
+String webHeadMonit="<html> <head> <meta http-equiv='refresh' content='1; url='http://localhost' method='get' /> <meta name='viewport' content='width=device-width, initial-scale=1.0'/> <meta http-equiv='Content-Type' content='text/html;charset=utf-8' /> <style> body { background: #eab0dc; font-family: \"Lato\", sans-serif; } .button { border: none; border-color:black; color: white; padding: 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; } .buttonMenu { padding: 5px 24px; margin-left:20%; background-color:black; border: none; border-color:black; color:white; text-align: left; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; } .sidenav { height: 100%; width: 0; position: fixed; z-index: 1; top: 0; left: 0; background-color: #111; overflow-x: hidden; transition: 0.5s; padding-top: 60px; } .sidenav a { padding: 8px 8px 8px 32px; text-decoration: none; font-size: 18px; color: #818181; display: block; transition: 0.3s; } .sidenav a:hover { color: #f1f1f1; } .sidenav .closebtn { position: absolute; top: 0; right: 25px; font-size: 36px; margin-left: 50px; } .button-on {border-radius: 100%; background-color: #4CAF50;} .button-off {border-radius: 100%;background-color: #707070;} .button-ledon {border-radius: 100%; padding: 10px; font-size: 8px; margin: 0px 0px; background-color: #ff4500;} .button-ledoff {border-radius: 100%; padding: 10px; font-size: 8px; background-color: #707070;} </style> </head> <body> <script> function openNav() { document.getElementById(\"mySidenav\").style.width = \"150px\"; } function closeNav() { document.getElementById(\"mySidenav\").style.width = \"0\"; } </script>";
+String webHead="<html> <head> <meta name='viewport' content='width=device-width, initial-scale=1.0'/> <meta http-equiv='Content-Type' content='text/html;charset=utf-8' /> <style> body { background: #eab0dc; font-family: \"Lato\", sans-serif; } .button { border: none; border-color:black; color: white; padding: 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; } .buttonMenu { padding: 5px 24px; margin-left:20%; background-color:black; border: none; border-color:black; color:white; text-align: left; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; } .sidenav { height: 100%; width: 0; position: fixed; z-index: 1; top: 0; left: 0; background-color: #111; overflow-x: hidden; transition: 0.5s; padding-top: 60px; } .sidenav a { padding: 8px 8px 8px 32px; text-decoration: none; font-size: 18px; color: #818181; display: block; transition: 0.3s; } .sidenav a:hover { color: #f1f1f1; } .sidenav .closebtn { position: absolute; top: 0; right: 25px; font-size: 36px; margin-left: 50px; } .button-on {border-radius: 100%; background-color: #4CAF50;} .button-off {border-radius: 100%;background-color: #707070;} .button-ledon {border-radius: 100%; padding: 10px; font-size: 8px; margin: 0px 0px; background-color: #ff4500;} .button-ledoff {border-radius: 100%; padding: 10px; font-size: 8px; background-color: #707070;} </style> </head> <body> <script> function openNav() { document.getElementById(\"mySidenav\").style.width = \"150px\"; } function closeNav() { document.getElementById(\"mySidenav\").style.width = \"0\"; } </script>";
+String webTail="</body> </html>";
+
+String webMenu="";
+String webHtml="";
+
+void handleRoot() {
+  makeHtml();
+  if(monit==1)
+    server.send(200, "text/html", webHeadMonit+webHtml+webTail);
+  else
+    server.send(200, "text/html", webHead+webHtml+webTail);
+}
+
+void makeHtml() {
+  String s=""; 
+
+  s+="<h3 style='color:SteelBlue;'>&emsp;김동일유튜브 <a href='http://i2r.link' style='color:SteelBlue;'>http://i2r.link</a> </h3>";
+
+  s+="모니터링 ";
+  s+="<form action='/on'>";
+  s+="<input type='hidden' name='act' value='1'>";
+  s+="<input type='hidden' name='no' value='0'>";
+  if(monit==1) 
+      s+="<th><button type='submit' name='value' value='0' class='button button-on' ></button></a></th>";
+  else
+      s+="<th><button type='submit' name='value' value='1' class='button button-off' ></button></a></th>";    
+  s+="</form>";
+
+  s+="<br>입력<table style='width:100%'><br>";
+  s+="<table>";
+  s+="<tr>";
+  s+="<th>00</th>";
+  s+="<th>01</th>";
+  s+="<th>02</th>";
+  s+="<th>03</th>";
+  s+="<th>04</th>";
+  s+="<th>05</th>";
+  s+="<th>06</th>";
+  s+="<th>07</th>";
+  s+="</tr>";
+
+  s+="<tr>";
+  for(int i=0;i<8;i++) {
+    s+="<th>";
+    if(In[i]==1) 
+      s+="<button class='button button-ledon' ></button></a>"; //off
+    else 
+      s+="<button class='button button-ledoff' ></button></a>"; //on
+    s+="</th>";
+  }
+  s+="<tr>";
+  s+="</table>";
+
+  s+="출력<br>";
+  s+="<table>";
+  s+="<tr>";
+  s+="<th>00</th>";
+  s+="<th>01</th>";
+  s+="<th>02</th>";
+  s+="<th>03</th>";
+  s+="<th>04</th>";
+  s+="<th>05</th>";
+  s+="</tr>";
+  s+="<tr>";
+  for(i=0;i<6;i++) {
+  s+="<form action='/on'>";
+  s+="<input type='hidden' name='act' value='2'>";
+  s+="<input type='hidden' name='no' value='"+(String)i+"'>";
+  if(Out[i]==1) 
+      s+="<th><button type='submit' name='value' value='0' class='button button-on' ></button></a></th>";
+  else
+      s+="<th><button type='submit' name='value' value='1' class='button button-off' ></button></a></th>";    
+  s+="</form>";
+  }
+  s+="</tr>";
+  s+="</table>";
+
+  s+="<br><br>AP & IP :&emsp;"+sMac+"&emsp;"+WiFi.localIP().toString();
+  webHtml=s;
+}
+
+// act 1=monitoring on/off  2=출력
+void handleOn() {
+  act=server.arg("act").toInt();
+  int no=server.arg("no").toInt();
+  int value=server.arg("value").toInt();
+
+  Serial.println("----------------------------");
+  Serial.println(act);
+  Serial.println(no);
+  Serial.println(value);
+
+  if(act==1) {
+    monit=value;
+  }
+  else if(act==2) {
+    Out[no]=value;
+    crd16Rtu();
+  }
+
+  GoHome();
+}
+
+void GoHome() {
+  String s;
+  s="<meta http-equiv='refresh' content=\"0;url='http://"+WiFi.localIP().toString()+"/'\">";
+  server.send(200, "text/html", s);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.send ( 404, "text/plain", message );
+}
---
+++ 501 wifi 485/plc_server_2_25_2/plc_server_2_25_2.ino
@@ -0,0 +1,148 @@
+// 다음 메뉴얼 참조하세요.
+// LS산전 사용설명서_XGB Cnet_V1.8.pdf 9-14 페이지 비트연속출력으로 프로그램 
+// crc16 rtu를 사용하기 위해 다음 라이브러리 설치하세요.
+// crc16 modbus
+//  https://github.com/RobTillaart/CRC
+//  AUTHOR: Rob Tillaart
+
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "CRC.h"
+#include <Ticker.h>
+#include <SoftwareSerial.h>
+
+Ticker ticker;
+
+SoftwareSerial mySerial(D7, D4); // RX, TX
+
+//json을 위한 설정
+StaticJsonDocument<200> doc;
+DeserializationError error;
+JsonObject root;
+
+ESP8266WebServer server(80);
+
+String sChipId="";
+char cChipId[40]="";
+int count=0,countScaned=0,actIp[255]={0};
+int ipNo; //현재 선택되어 동작중인 IP
+
+String inputString = "";    // 받은 문자열
+int sendText=0,errorPlc=0;
+int Out[8]={0},In[10]={0};  // plc 입력과 출력 저장 
+int monit=0;                //모니터링 on/off
+int act=0;
+
+char msg[100];
+int i=0;
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
+
+void tick()
+{
+  crd16Rtu();
+  act=1;
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  Serial.begin(19200);
+  mySerial.begin(19200);
+  WiFiManager wm;
+ 
+  // AP 이름 자동으로 만듬 i2r-chipid
+  sChipId = "i2r-"+String(ESP.getChipId(),HEX);
+  sChipId.toCharArray(cChipId,sChipId.length()+1);
+  Serial.println(sChipId);
+
+  //wifi manager 참조유튜브  https://youtu.be/mJFd7g4jedw  //https://youtu.be/EzEG_2HcOQo
+  //reset settings - wipe credentials for testing
+  //wm.resetSettings();
+  if (!wm.autoConnect(cChipId)) {
+    Serial.println("failed to connect and hit timeout");
+    ESP.restart();
+    delay(1000);
+  }
+  Serial.print("connected... :");
+  Serial.print(WiFi.localIP().toString());
+
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.onNotFound(handleNotFound);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+
+  ticker.attach(5, tick);  //0.1 초도 가능
+  //ticker.detach();
+}
+
+void loop() {
+  server.handleClient();
+  serialEvent();
+}
+
+void serialEvent() {
+  if(mySerial.available() == false)
+    return;
+  while (mySerial.available()) {
+    // get the new byte:
+    char inChar = (char)mySerial.read();
+    Serial.print(inChar,HEX);
+    // add it to the inputString:
+    inputString += inChar;
+  }
+  Serial.println("");
+  if(monit==1 && act==1 && inputString.length() >= 6) {
+    int b=1;
+    for(int i=1;i<=8;i++) {
+        int c=inputString.charAt(3)&b;
+        if(c!=0)
+          c=0x01;
+        In[i-1]=c;
+        Serial.print(c,HEX);
+        Serial.print(" ");
+        b*=2;
+      }
+    inputString="";
+    Serial.println("");
+  }
+}
+
+// 아두이노에서 RS485 출력을 내보낸다.
+void crd16Rtu() {
+  String s;
+  int si,sj,len;
+  char str[24];
+
+  if(act == 2) {
+    //str[24] =  {0x00,0x0f,0x00,0x00,0x00,0x0a,0x02,0xff,0x00,0x00,0x00};  //비트연속출력 len=9
+    str[0]=0x00; str[1]=0x0f; str[2]=0x00; str[3]=0x00; str[4]=0x00;
+    str[5]=0x0a; str[6]=0x02; str[7]=0xff; str[8]=0x00; str[9]=0x00; str[10]=0x00;
+    len=9;
+    str[7]=Out[0]+Out[1]*2+Out[2]*4+Out[3]*8+Out[4]*16+Out[5]*32;
+  }
+  else if(monit==1) {
+    //str[24] =  {0x00,0x02,0x00,0x00,0x00,0x08,0x00,0x00}; // 비트 입력영역 읽기 len=6
+    str[0]=0x00; str[1]=0x02; str[2]=0x00; str[3]=0x00; str[4]=0x00;
+    str[5]=0x08; str[6]=0x00; str[7]=0x00; 
+    len=6;
+    act=1;
+  }
+  else
+    return;
+
+  inputString = "";
+  uint8_t * data = (uint8_t *) &str[0];
+  si=crc16(data, len, 0x8005, 0xFFFF, 0x0000, true,  true  );
+  sj=si&0xff;
+  str[len]=sj;
+  sj=si>>8;
+  str[len+1]=sj;
+
+  for(int i=0;i<len+2;i++)
+    mySerial.print(str[i]);
+}
